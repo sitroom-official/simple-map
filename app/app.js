878a280ab40b0e2e1d7648d2957172f44845a28d
norbigith <dulnorbert@gmail.com>
2022-03-23 01:08:34 +0100

    Completed loadUsers function
    i did not deleted the comments yet i made for myself to help solving the
    task

--- app/app.js
+++ app/app.js
@@ -36,12 +36,78 @@ const App = () => {
         })
     }
 
-    useEffect( function loadUsers() {
+    useEffect( 
+        
+        function loadUsers() { //this function is the first argument
         if(!usersList?.[0]?.firstName && !isFetching) {
             // your code here
+            //this was line 42
+
+           /*probálgatás előtt
+            beforeRequestUser();//első
+            //valószínülg a fetch datat azzall hívom meg amit láttunk a quizben, azz lesz a masodik
+            fetchUserData(userId); //harmadik
+            afterReceiveUser(user);// i dont know where im gonna get the user yet probably from fetch
+            //probably i need to put  afterReceiveUser(user) into a for loop that loop through
+            // the users and pass from userdata array one array elemnt
+            //or maybe eleve a fetch után hívom meg a quizes functionba es onna passelek bele
+           //uj gondolat
+            //mivel 3 fetchet csinálok es ennek after fetch a neve emiat beleteszem a fetch mellé
+            //és nem az arrayből rakok ide a parameternek hanem egyből a fetched datat rakom proba
+            //and also after recive user and not users shows i should use it after every fetch?
+            //az a főbb kérdés, hogy mit kell pontosan belepasszolni
+            //a fetched datat vagy abból egy részt?
+            
+
+            addMarkerToMap(color, coords);
+            //nem tudom pontosan hol hívjam meg, hogy itt vagy egy masik functionbe, még megnézem
+            //a user datábol mind a colort mind a coordsot megkapom
+            */
+
+            beforeRequestUser();//első
+
+            const usersIds = [{userId : 1}, {userId : 2},{userId : 3}];
+
+           
+            
+            let allUsersData = [];
+
+            async function doStuff(){
+            
+                for(let index = 0; index < usersIds.length; index++) {
+                    let userId=usersIds[index].userId;
+                    try {
+            
+                        const userData = await fetchUserData(userId);//masodik
+            
+                        let color=userData.data.color;
+                        let coords=userData.data.coords;
+                        allUsersData.push(userData);
+                        afterReceiveUser(userData);
+            
+                        addMarkerToMap(color, coords);
+            
+                    } catch(error) {
+            
+                        console.log('failed to fetch', userId, error);
+            
+                    }
+            
+                }
+            
+                // if I do console.log(allUsersData ) here it will contain the users information
+                console.log(allUsersData );
+            }
+            
+            doStuff();
+            
+
+
 
         }
-    }, [usersList, isFetching])
+    }, [usersList, isFetching]//this is the second argument
+    
+    )
 
     return html`
     <style>
